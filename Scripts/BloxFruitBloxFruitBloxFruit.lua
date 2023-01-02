a01307d3a7d2329e04e2f0bf81fa5b298c2746d1
A1phes <121830383+A1phes@users.noreply.github.com>
2023-01-03 04:51:18 +0700

    Update BloxFruitBloxFruitBloxFruit.lua

--- Scripts/BloxFruitBloxFruitBloxFruit.lua
+++ Scripts/BloxFruitBloxFruitBloxFruit.lua
@@ -1,4 +1,4 @@
-local Game = "BloxFrui"
+local Game = "BloxFruit"
 if decoded == nil or not rawequal(decoded["status"],"success") or decoded["message"] ~= Game .. Game .. Game then
 	repeat wait() until false
 end
