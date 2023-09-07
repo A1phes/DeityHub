d8692e4939dc7da07fc62c4daa01ed04e7144fa0
A1phes <121830383+A1phes@users.noreply.github.com>
2023-09-07 23:33:08 +0700

    Update Xenon_Theme.lua

--- UI/Linoria/Xenon_Theme.lua
+++ UI/Linoria/Xenon_Theme.lua
@@ -4,8 +4,10 @@ local ThemeManager = {} do
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
-  ThemeManager.BuiltInThemes = {
-    ['Xenon Hub'] 		= { 1, httpService:JSONDecode('{"MainColor":"191919","AccentColor":"ff4d4d","OutlineColor":"323232","BackgroundColor":"1c1c1c","FontColor":"ffffff"}') },
+    ThemeManager.BuiltInThemes = {
+        ['Xenon Hub V1'] 		= { 1, httpService:JSONDecode('{"MainColor":"000f1e","AccentColor":"00b4f0","OutlineColor":"0a1e28","BackgroundColor":"050514","FontColor":"ffffff"}') },
+        ['Xenon Hub V2'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"181825","AccentColor":"ff3d3d","BackgroundColor":"16161f","OutlineColor":"323232"}') },
+        ['Deity Hub - Next Gen'] 		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0EDEFF","BackgroundColor":"141414","OutlineColor":"323232"}') }
 	}
 
 	function ThemeManager:ApplyTheme(theme)
