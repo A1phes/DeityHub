491a3dabff656a8b44e4fc34c16e7fb3f2b544c2
A1phes <121830383+A1phes@users.noreply.github.com>
2023-11-24 19:45:56 +0700

    Update Library.lua

--- UI/Linoria/Library.lua
+++ UI/Linoria/Library.lua
@@ -15,7 +15,14 @@ local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
-ScreenGui.Parent = CoreGui;
+
+if game.GameId == 4730278139 then
+    ScreenGui.Parent = LocalPlayer.PlayerGui;
+else
+    ScreenGui.Parent = CoreGui;
+end
+
+
 
 local Toggles = {};
 local Options = {};
@@ -3152,7 +3159,7 @@ function Library:CreateWindow(...)
             BackgroundTransparency = 1;
             BorderSizePixel = 0;
             Position = UDim2.new(0.5, 4 + 1, 0, 8 - 1);
-            Size = UDim2.new(0.5, -12 + 2, 0.95, 0); -- Size = UDim2.new(0.5, -12 + 2, 0, 507 + 2);
+            Size = UDim2.new(0.5, -12 + 2, 0.95, 0);-- Size = UDim2.new(0.5, -12 + 2, 0, 507 + 2);
             CanvasSize = UDim2.new(0, 0, 0, 0);
             BottomImage = '';
             TopImage = '';
